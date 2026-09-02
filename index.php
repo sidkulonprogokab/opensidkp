60fcdaa50c4636c7fb84bc01e3e8ed4761c91d37
kozinF <mkozin.work@gmail.com>
2026-09-02 14:37:23 +0700

    Tambah script di index.php untuk otomatisasi site_url

--- index.php
+++ index.php
@@ -36,6 +36,19 @@
  */
 
 /*
+* 
+* Ditambahkan agar tidak perlu setting base url untuk yang diinstall di container
+*
+*/
+
+if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+}
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+/*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
  *---------------------------------------------------------------
